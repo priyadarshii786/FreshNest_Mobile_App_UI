23c2bb8f2cada5aee7cc8014d5895569d7fdbc5b
priyadarshii786 <tathagatp918@gmail.com>
2024-08-05 01:49:29 +0530

    C-02 commit to observe the forked repo

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
-              // observing the sync from forked repo
+              // observing the sync from forked repo....
             ),
             Text(
               '$_counter',
