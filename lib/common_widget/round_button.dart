f8dc4f5b4437e385f3a9a1aabb8339f5edc0fc6e
priyadarshii786 <tathagatp918@gmail.com>
2024-08-16 03:36:13 +0530

    C-08; Completed the 3rd screen ie; Sign_In Screen.

--- lib/common_widget/round_button.dart
+++ lib/common_widget/round_button.dart
@@ -25,3 +25,48 @@ class RoundButton extends StatelessWidget {
     );
   }
 }
+
+class RoundIconButton extends StatelessWidget {
+  final String title;
+  final String icon;
+  final Color bgColor;
+  final VoidCallback onPressed;
+
+  const RoundIconButton(
+      {super.key,
+      required this.title,
+      required this.icon,
+      required this.bgColor,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      // Important parameters of the "Get Started" button.
+      onPressed: onPressed,
+      height: 60,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
+      minWidth: double.maxFinite,
+      elevation: 0.1,
+      color: bgColor,
+      child: Row(
+        children: [
+          Image.asset(
+            icon,
+            width: 20,
+            height: 20,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+                color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/view/splash_view.dart
+++ lib/view/splash_view.dart
@@ -44,7 +44,7 @@ class _SplashViewState extends State<SplashView> {
     // Important parameters of sketching the splash screen.
     var media = MediaQuery.sizeOf(context);
     return Scaffold(
-      backgroundColor: TColor.primary,
+      backgroundColor: TColor.darkGray,
       body: Center(
         child: Image.asset("assets/img/splash_logo.png",
             width: media.width * 0.5), //Splash logo
